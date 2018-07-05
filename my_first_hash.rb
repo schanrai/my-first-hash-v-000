2e320097c7fa16af234b2e853e4877016d489f10
Sushi Chanrai <sushi@pathworks.co>
2018-07-05 23:18:05 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,6 +17,8 @@ def shipping_manifest
 
 end
 
+
+
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
@@ -28,6 +30,8 @@ def retrieval
 shipping_manifest["oil paintings"]
 end
 
+
+
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
@@ -35,7 +39,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
